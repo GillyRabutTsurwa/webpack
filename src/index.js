57cf4db06bc12dfbf60199943750b2245c92712d
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-09-28 12:08:42 -0400

    Lesson 2.1: Loading Images - the generator object is specifically useful if you want to put the webpack generated image inside a specific folder - this is because webpack automatically searches for the images in the root (dist) folder. but thankfully we can change it - this keeps our dist folder clean and looking as good as our src folder - we mooo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import "./style.css";
+import "./img/logo.png";
 console.log("Bonjour depuis Webpack");
--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,20 @@ module.exports = {
     },
     module: {
         rules: [
+            //NOTE: loading css stylesheets
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
+            },
+            //NOTE: loading image files
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+                //NOTE: grâce à çe code la-dessous, l'image généré par webpack sera mise dans la dossier, qui fait plus propre et organisé le dossier dist
+                generator: {
+                    publicPath: "img/",
+                    outputPath: "img/",
+                },
             },
         ],
     },
