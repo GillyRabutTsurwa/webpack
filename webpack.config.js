b7555567c8bd030ce9eac81642a06dfd07c8059c
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-11-24 20:57:27 -0500

    Lesson 4.0: Development - Sourcemaps - important refresher, as i've been reading on sourcemaps a lot lately - not much to the lesson though - we mooo

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,23 @@ const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    //NEWNOTE: interesting. so the entry property can also be an object.
-    // i assume the property (of the entry object) is the file name that webpack will use to bundle
-    // and the value is obviously the path
+    mode: "development",
     entry: {
         index: "./src/js/index.js",
         print: "./src/js/print.js",
     },
+    devtool: "inline-source-map",
     plugins: [
         new HTMLWebpackPlugin({
-            title: "Output Management Deez Nuts",
+            title: "Development",
             template: "./src/index.html",
             filename: "index.html", //NOTE: not needed because it generates index.html by default, but good to know in case you need a different name
         }),
     ],
     output: {
-        //NOTE: i think [name] here will work because we made entry an object with specific properties
-        //NOTE: so i think [name] will equate to index or print (the properties in the entry object).
-        //NOTE: i'm not sure yet. voyons bien
-        filename: "js/[name].bundle.js", //NEW: changing this so all the generated javascript files are in a js folder instead
+        filename: "js/[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
-        clean: true, //NEW: ce code ceci nettoyera le dossier "dist" avant chaque build. TESTING: ça marche. PASS
+        clean: true,
     },
     module: {
         rules: [
